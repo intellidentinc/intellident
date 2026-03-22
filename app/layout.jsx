acffac935c91eb163acf4a6dab4447882c0a5c2f
JPAVictoria <jpavictoria829@gmail.com>
2026-03-22 23:32:15 +0800

    feat: initial commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,9 @@
-import { Inter } from "next/font/google";
+import { Inter, Geist } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const inter = Inter({
   subsets: ["latin"],
@@ -10,7 +13,7 @@ const inter = Inter({
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className={`${inter.variable} antialiased`}>
         {children}
         <Analytics />
