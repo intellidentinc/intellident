0b83cf29e9cd902723a9b12c9676aebb9dea3684
JPAVictoria <jpavictoria829@gmail.com>
2026-05-05 21:19:36 +0800

    g

--- app/(main)/reset-password/page.jsx
+++ app/(main)/reset-password/page.jsx
@@ -1,3 +1,13 @@
+import { Suspense } from 'react';
 import ResetPasswordPage from '@/app/modules/reset-password-page/ResetPasswordPage';
+
+export const dynamic = 'force-dynamic';
 export const metadata = { title: 'Reset Password | IntelliDent' };
-export default function Page() { return <ResetPasswordPage />; }
+
+export default function Page() {
+  return (
+    <Suspense>
+      <ResetPasswordPage />
+    </Suspense>
+  );
+}
