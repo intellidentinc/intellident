39e5115c9cb7d44333971c3502af702c40fdfbdd
JPAVictoria <jpavictoria829@gmail.com>
2026-05-05 21:14:47 +0800

    g

--- app/(main)/verify-otp/page.jsx
+++ app/(main)/verify-otp/page.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react'
 import VerifyOtpPage from '@/app/modules/verify-otp-page/VerifyOtpPage'
 
+export const dynamic = 'force-dynamic'
 export const metadata = { title: 'Verify Sign-In | IntelliDent' }
 
 export default function Page() {
