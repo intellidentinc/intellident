267bbd45770817f3da70dce991d90d940a60f431
Scoowie <armamentow@gmail.com>
2026-06-08 04:25:03 +0800

    fix: patched build error

--- app/(main)/step-up/page.jsx
+++ app/(main)/step-up/page.jsx
@@ -1,7 +1,12 @@
+import { Suspense } from 'react';
 import StepUpPage from '@/app/modules/step-up-page/StepUpPage';
 
 export const metadata = { title: 'Verify Identity | IntelliDent' };
 
 export default function Page() {
-  return <StepUpPage />;
+  return (
+    <Suspense>
+      <StepUpPage />
+    </Suspense>
+  );
 }
