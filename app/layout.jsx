0c4a2aaf627bcec0d4eec59aa278816b8a728a06
Scoowie <armamentow@gmail.com>
2026-06-17 04:46:53 +0800

    feat: added vercel speed insight

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter, Geist } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
       <body className={`${inter.variable} antialiased`}> 
         {children} 
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
